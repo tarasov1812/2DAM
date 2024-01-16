466647606a827285e4ea581b343321a86d96ed77
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-01-16 19:58:32 +0100

    ejercicio hecho

---
+++ 2 Trimestre/PMDM/EjercicioLayouts8DeEnero/MyApplication8Enero/app/src/main/java/com/example/clase9DeEnero/MainActivity3Clase9DeEnero.kt
@@ -0,0 +1,12 @@
+package com.example.clase9DeEnero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class MainActivity3Clase9DeEnero : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity3_clase9_de_enero)
+    }
+}
---
+++ 2 Trimestre/PMDM/EjercicioLayouts8DeEnero/MyApplication8Enero/app/src/main/java/com/example/clase9DeEnero/MainActivity4Clase9DeEnero.kt
@@ -0,0 +1,12 @@
+package com.example.clase9DeEnero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class MainActivity4Clase9DeEnero : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity4_clase9_de_enero)
+    }
+}
